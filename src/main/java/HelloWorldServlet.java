f01660084045bab1c06790b7ae0729d04c123379
Jeremy Sellers <jeremy.c.sellers@gmail.com>
2022-04-26 08:58:05 -0500

    Servlets exercise

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,9 +1,17 @@
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
+@WebServlet("/hello")
 public class HelloWorldServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest req, HttpServletResponse res) {
+
+    private String myName = "Jeremy Sellers";
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+        res.getWriter().println("Hello, World!"); //after rerunning tomcat, this class prints "Hello World" to screen when /hello at end of URL
 
     }
 }
